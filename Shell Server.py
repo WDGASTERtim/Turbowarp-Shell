f7dbf7917541d9d1102668ce7f85ffbe9c32ee09
WDGASTERtim <108210308+WDGASTERtim@users.noreply.github.com>
2023-04-28 02:44:48 +0300

    Add Safe-UnSafe Mode

--- Shell Server.py
+++ Shell Server.py
@@ -1,19 +1,29 @@
-import asyncio
 import os
 import websockets
+import asyncio
 
 async def handle_command(command):
-    # Выполнить команду в консоли и получить результат
     result = os.system(command)
     print(result)
 
 async def websocket_handler(websocket, path):
+    if unsafe_mode == 1:
+        print('New client accepted')
+    else:
+        accept_client = input('Accept a new client? (y/n): ')
+        if accept_client.lower() != 'y':
+            print('Client rejected')
+            return
+        else:
+            print('New client accepted')
+    
     async for message in websocket:
-        # Обработать полученное сообщение как команду
         await handle_command(message)
 
 async def main():
     async with websockets.serve(websocket_handler, 'localhost', 8765):
-        await asyncio.Future()  # Ждем подключений
+        await asyncio.Future()
+
+unsafe_mode = 0 #CHANGE THIS TO ENABLE UNSAFE MOD ALL CLIENTS WILL BE AUTOMAICLY ACCEPTED
 
 asyncio.run(main())
